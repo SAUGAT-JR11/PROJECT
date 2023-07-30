b014b0acd7e1c7309373cc750235463e35f2eb37
Karina paudel <karinapaudel6@gmail.com>
2023-07-30 15:23:59 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".home-slider", {
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    
+});
