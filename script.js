9a342f5cb31910b43a810d614390160fe4445747
Karina paudel <karinapaudel6@gmail.com>
2023-08-01 18:07:58 +0545

    second commit

--- script.js
+++ script.js
@@ -8,3 +8,28 @@ var swiper = new Swiper(".home-slider", {
     },
     
 });
+var swiper = new Swiper(".room-slider", {
+    spaceBetween: 20,
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el:".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 1,
+        },
+    }
+});
